6b053c2a148fbbc6f5c781a4652b99dc79ec1820
Ilya <iikirmenev@avito.ru>
2024-07-06 09:23:32 +0300

    firs commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+	const [message, setMessage] = useState('');
+
+	useEffect(() => {
+		fetch('/api')
+			.then(response => response.json())
+			.then(data => setMessage(data.message));
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>{message}</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
+
+
+
+
